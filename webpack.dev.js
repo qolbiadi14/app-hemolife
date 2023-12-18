22a702d56cae59b42d6d11dcfb8b8025ff784697
Your Name <your.email@example.com>
2023-12-18 13:50:00 +0700

    ganti port

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     static: path.resolve(__dirname, 'dist'),
-    port: 3000,
+    port: 5000,
     open: true,
     compress: true,
     client: {
